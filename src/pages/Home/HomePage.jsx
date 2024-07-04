b57e1867b54ddd0556fed22ea5cb28a580efbde0
ok-qa <olechka.kr@gmail.com>
2024-07-03 20:37:09 -0700

    homePage: corrected name

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,8 +3,11 @@ import css from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <div className={css.wrapper}>
-      <h1 className={css.title}>WELCOME</h1>
-      <h2 className={css.title}>to the Phonebook</h2>
+      <h1 className={css.title}>
+        WELCOME
+        <br />
+        to the Phonebook
+      </h1>
     </div>
   );
 }
